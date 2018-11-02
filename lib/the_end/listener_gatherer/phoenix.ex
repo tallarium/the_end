b1e70e7cb9c77040c5e6698293da89dd24ac52c4
Michal Szewczak <michal@szewczak.email>
2018-11-02 21:25:59 +0000

    change examined supervisor to the Endpoint itself

--- lib/the_end/listener_gatherer/phoenix.ex
+++ lib/the_end/listener_gatherer/phoenix.ex
@@ -5,8 +5,7 @@ defmodule TheEnd.ListenerGatherer.Phoenix do
   import Supervisor, only: [which_children: 1]
 
   def gather(endpoint) do
-    endpoint_server = Module.concat(endpoint, Server)
-    for {{:ranch_listener_sup, _}, pid, _, _} <- which_children(endpoint_server), do: pid
+    for {{:ranch_listener_sup, _}, pid, _, _} <- which_children(endpoint), do: pid
   end
 
 end
